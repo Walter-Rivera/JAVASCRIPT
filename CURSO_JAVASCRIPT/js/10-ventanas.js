039600067448f1c9c3279ae76e3cf83474cd30ac
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-17 22:59:12 -0600

    agregando archivo js 10-ventajas.js

---
+++ CURSO_JAVASCRIPT/js/10-ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//alertas
+//alert("mensaje");
+
+/*confirmación
+var resultado = confirm("¿Desea continuar？");
+console.log(resultado);*/
+
+//ingreso de datos
+var datos = prompt("¿Qué número ingresará？",0);
+console.log(datos);
