bc50e2f48de151fce4fc42002bacec2c2a4e4b9a
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-18 01:09:04 -0600

    agregando modificaciones al archivo 11-ejercicio1.js

--- CURSO_JAVASCRIPT/js/11-ejercicio1.js
+++ CURSO_JAVASCRIPT/js/11-ejercicio1.js
@@ -1,11 +1,18 @@
 'use strict'
 
 /*programa que pida dos numeros y determine el mayor, menor o si son iguales*/
+/*funcion adicional: Si alguno de los dos datos no es numérico y si están por debajo de 0, se pidan 
+nuevamente los datos al usuario*/
 
-var val1 = parseFloat(prompt("ingrese primer numero",0));
-var val2 = parseFloat(prompt("ingrese segundo numero",0));
 
-    if(val1>val2)
+do{
+    var val1 = parseFloat(prompt("ingrese primer numero",0));
+    var val2 = parseFloat(prompt("ingrese segundo numero",0));
+    if(val1<0 ||val2<0)
+    {   
+        alert("uno de los dos números que ingresó es menor a 0, por favor intente de nuevo");
+    }
+    else if(val1>val2)
     {
         alert("el numero    "+val1+"    es el mayor");
     
@@ -22,3 +29,4 @@ var val2 = parseFloat(prompt("ingrese segundo numero",0));
     {
         alert("ingresó un dato que no es numérico");
     };
+}while(val1<0 || val2<0||isNaN(val1)|| isNaN(val2));    
