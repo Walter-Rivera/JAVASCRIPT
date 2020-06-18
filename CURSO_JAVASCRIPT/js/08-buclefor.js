483c97cb973baae2b102ae298a3329df24c51f7b
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-17 22:42:59 -0600

    agregando ficheros 08, 09 bucles for y while en js

---
+++ CURSO_JAVASCRIPT/js/08-buclefor.js
@@ -0,0 +1,11 @@
+'use strict'
+//bucle for
+
+var contador=100;
+for(var i=0;i<101;i++)
+{
+    console.log(i);
+    /*debugger nos permite ver durante la  ejecucición del programa el valor 
+    que llevan nuestras variables, pero debemos llamarlas desde la parte de watch*/ 
+    /*debugger;*/
+};
---
+++ CURSO_JAVASCRIPT/js/09-buclewhile.js
@@ -0,0 +1,11 @@
+'use strict'
+/*ciclo while en js*/
+
+
+var numero=0;
+
+while(numero<101)
+{
+    console.log(numero);
+    numero++;
+};
