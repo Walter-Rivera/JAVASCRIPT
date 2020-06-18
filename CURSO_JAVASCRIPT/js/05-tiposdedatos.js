0ca3ec63c92ef997d29a843896038b7ac14056e1
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-17 19:24:17 -0600

    agregando archivo 05.-tiposdedatos.js

---
+++ CURSO_JAVASCRIPT/js/05-tiposdedatos.js
@@ -0,0 +1,22 @@
+'use strict'
+/*operadores*/
+var numero1 = 100;
+var numero2= 200;
+
+var suma = numero1+numero2;
+var resta=numero2-numero1;
+var multiplicacion= numero1*numero2;
+var division = numero1/numero2;
+
+alert("el resultado de la suma es:    "+suma+"\n"
++ "el resultado de la resta es:    "+resta+"\n"
++ "el resultado de la multiplicación es:    "+multiplicacion+"\n"
++ "el resultado de la división es:     "+division+"\n"
+);
+
+/*funciones para convertir tipos de datos
+number();
+String();
+parseint();
+parsefloat();
+*/
