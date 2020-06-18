aaec02396438adb5ca78fe910b19a844db09882a
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-17 19:26:55 -0600

    agregando la funcion typeof en js

--- CURSO_JAVASCRIPT/js/05-tiposdedatos.js
+++ CURSO_JAVASCRIPT/js/05-tiposdedatos.js
@@ -2,7 +2,7 @@
 /*operadores*/
 var numero1 = 100;
 var numero2= 200;
-
+var cadena = "uno dos tres";
 var suma = numero1+numero2;
 var resta=numero2-numero1;
 var multiplicacion= numero1*numero2;
@@ -13,6 +13,11 @@ alert("el resultado de la suma es:    "+suma+"\n"
 + "el resultado de la multiplicación es:    "+multiplicacion+"\n"
 + "el resultado de la división es:     "+division+"\n"
 );
+
+/*typeof es una funcion que nos permite saber el tipo de dato
+de una variable*/
+console.log(typeof(cadena));
+
 
 /*funciones para convertir tipos de datos
 number();
