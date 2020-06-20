38aaa117ef491311b97b22e4b70c5e2aa45efe81
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-20 16:10:58 -0600

    agregando la funcion de calculadora al archivo

--- CURSO_JAVASCRIPT/js/19-funciones.js
+++ CURSO_JAVASCRIPT/js/19-funciones.js
@@ -2,6 +2,8 @@
 
 /*funciones en js*/
 
+
+/*
 function mostrar_texto()
 {
     return "esta es la función";
@@ -9,3 +11,31 @@ function mostrar_texto()
 };
 
 console.log(mostrar_texto());
+*/
+
+/*funcion para hacer una calculadora dinámica */
+
+
+function calculadora(numero1,numero2)
+{
+    var resultados=`la suma entre los dos números es: ${numero1+numero2}
+        la resta entre los dos números es: ${numero1-numero2}
+        la multiplicación entre los dos números es: ${numero1*numero2}
+        la division entre los dos números es: ${numero1/numero2}
+    `;
+    return resultados;
+}
+
+
+var dato1 = parseFloat(prompt("ingrese el primer número",0));
+var dato2= parseFloat(prompt("ingrese el segundo número",0));
+
+while(isNaN(dato1)|| isNaN(dato2))
+{
+     dato1 = parseFloat(prompt("ingrese el primer número",0));
+     dato2= parseFloat(prompt("ingrese el segundo número",0));
+
+}
+alert(calculadora(dato1,dato2));
+
+
