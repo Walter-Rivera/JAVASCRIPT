defc380dfc5175743786eae13f0a70eb2ada957b
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-19 23:28:34 -0600

    agregando archivo ejercicio07.js

---
+++ CURSO_JAVASCRIPT/js/17-ejercicio07.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*hacer la tabla de multiplicar de un numero introducido por pantalla*/ 
+
+var numero = parseInt(prompt("ingrese un número"),0);
+
+
+for(var i=0;i<11;i++)
+{
+    console.log(i*numero);
+
+};
