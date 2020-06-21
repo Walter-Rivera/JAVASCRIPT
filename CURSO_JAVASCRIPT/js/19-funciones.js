c7f95d07657aa01123fee223d6f5e0a3388a91b3
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-20 23:44:26 -0600

    agregando modificaciones a la funcion, para que reciba un parámetro opcional  que interactúa como flag

--- CURSO_JAVASCRIPT/js/19-funciones.js
+++ CURSO_JAVASCRIPT/js/19-funciones.js
@@ -16,16 +16,28 @@ console.log(mostrar_texto());
 /*funcion para hacer una calculadora dinámica */
 
 
-function calculadora(numero1,numero2)
+function calculadora(numero1,numero2,mostrar=false)
 {
-    var resultados=`
+    if(mostrar==false)
+    {
+        console.log("la suma entre los  2 numeros es:    "+(numero1+numero2));
+        console.log("la resta entre los 2 numeros es:    "+(numero1-numero2));
+        console.log("la multiplicación entre los 2 números es:    "+(numero1*numero2));
+        console.log("la division entre los 2 números es:   "+(numero1/numero2));
+
+    }
+    else
+    {
+        var resultados=`
         la suma entre los dos números es: ${numero1+numero2}
         la resta entre los dos números es: ${numero1-numero2}
         la multiplicación entre los dos números es: ${numero1*numero2}
         la division entre los dos números es: ${numero1/numero2}
-    `;
-    return resultados;
-}
+        `;
+        alert(resultados) ;
+    };
+    
+};
 
 
 var dato1 = parseFloat(prompt("ingrese el primer número",0));
@@ -37,6 +49,15 @@ while(isNaN(dato1)|| isNaN(dato2))
      dato2= parseFloat(prompt("ingrese el segundo número",0));
 
 }
-alert(calculadora(dato1,dato2));
+/* parámetros opcionales, podemos usar
+esta clase de funciones en donde los booleanos actuan como flags,
+en este caso sino enviamos el tercer parámetro, nos mostrará  por consola el resultado,
+en otro caso si le enviamos parametro y este es verdadero, nos devolverá en un alert el resultado
+*/
+
+/*alert(calculadora(dato1,dato2,true));*/
+
+var datos=calculadora(dato1,dato2);
+
 
 
