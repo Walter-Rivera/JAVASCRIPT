840ccea6c33534aa94796d83ae7dd7314e19caab
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-24 00:49:17 -0600

    ciclo for each

--- CURSO_JAVASCRIPT/js/25-arrays.js
+++ CURSO_JAVASCRIPT/js/25-arrays.js
@@ -2,5 +2,39 @@
 
 var nombres = ["nombre 1", "nombre 2", "nombre 3", "nombre N"];
 var lenguajes = new Array("PHP","JAVA","C++","GO");
-console.log(nombres);
-console.log(lenguajes);
+
+/*
+var elemento= parseInt(prompt("Qué elemento del array desea ver?"));
+
+if(elemento>=nombres.length)
+{
+    alert("introduce un número menor a :   "+nombres.length);
+
+}
+else
+{
+    alert(nombres[elemento]);
+}
+*/
+
+/*recorrer un array*/
+
+/*
+for (var i =0;i<lenguajes.length;i++)
+{
+    document.write("<ul>");
+        document.write("<li> "+lenguajes[i]+ " </li>");
+        document.write("<br/>");
+    document.write("</ul>");
+
+} ;
+*/
+
+
+/*Ciclo FOR EACH */
+
+lenguajes.forEach((elemento, indice,arreglo)=>
+{
+    document.write("<li> "+indice+"  "+elemento+ " </li>");
+
+});
