234bf4b7e6fe120432828c0bb6971deaee1cc11b
WalterRivera <giovannirivera1994.1@gmail.com>
2020-06-28 00:20:10 -0600

    agregando como modificar elementos del dom a través de us id o de su tagname(etiqueta)

--- CURSO_JAVASCRIPT/js/28-dom.js
+++ CURSO_JAVASCRIPT/js/28-dom.js
@@ -4,9 +4,7 @@ function cambiarColor(color)
 {
     elemento.style.background = color;
 };
-
-
-
+/*modificando elementos a través del id */
 var objeto = document.getElementById("elemento");
 objeto.innerHTML="nuevo texto desde js";
 objeto.style.background="blue";
@@ -14,4 +12,27 @@ objeto.style.padding="15px";
 objeto.style.color="white";
 objeto.className="Hola";
 
-console.log(objeto);
+
+
+
+/* conseguir elemetos POR SU etiqueta*/
+var divs = document.getElementsByTagName('div');
+
+var seccion = document.querySelector("#miSeccion");
+var hr = document.createElement("hr");
+
+var valor;
+
+for(valor in divs)
+{
+    if(typeof divs[valor].textContent=='string')
+    {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(divs[valor].textContent);
+        parrafo.append(texto);
+        seccion.append(parrafo);
+    }
+
+}
+seccion.append(hr);
+
